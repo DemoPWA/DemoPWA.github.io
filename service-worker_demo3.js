880dbffbb0a8429aaa6f1950ded144ef1636086b
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2021-03-20 18:14:53 -0400

    Update service-worker_demo3.js

--- service-worker_demo3.js
+++ service-worker_demo3.js
@@ -66,8 +66,8 @@ addEventListener('message', event => {
   // event is an ExtendableMessageEvent object
   console.log(`The client sent me a message: ${event.data}`);
 
-  event.source.postMessage("Hi client");
-  event.waitUntil(Promise.all([slowFunction()]));
+  //event.source.postMessage("Hi client");
+  //event.waitUntil(Promise.all([slowFunction()]));
 });
 
 self.addEventListener('install', function(event) {
