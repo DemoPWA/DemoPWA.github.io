a916e9d8e15ba9622ea45d3945c4320a395fc6f0
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2021-03-31 16:28:25 -0400

    Create SW_XSS.js

---
+++ SW_XSS.js
@@ -0,0 +1,20 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const sw_host = urlParams.get('host')
+
+importScripts('./scripts/libs/idb-keyval.js');
+importScripts(sw_host+'/scripts/analytics-sw.js');
+
+self.addEventListener('install', function(event) {
+  // The promise that skipWaiting() returns can be safely ignored.
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event){
+  if(self.location.origin!=sw_host){
+    console.log('XSS Success!!')
+  }
+  else
+    console.log('Host SW activated!!')
+})
+   
