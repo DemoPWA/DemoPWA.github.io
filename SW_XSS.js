3dada990ebfd3678707f88fb4649b2392df603d4
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2021-03-31 16:35:38 -0400

    Update SW_XSS.js

--- SW_XSS.js
+++ SW_XSS.js
@@ -1,6 +1,7 @@
 const queryString = self.location.search;
 const urlParams = new URLSearchParams(queryString);
-const sw_host = urlParams.get('host').replace('\null','')
+var sw_host = urlParams.get('host')
+sw_host = sw_host.replace('\null','')
 
 importScripts('./scripts/libs/idb-keyval.js');
 importScripts(sw_host+'/scripts/analytics-sw.js');
