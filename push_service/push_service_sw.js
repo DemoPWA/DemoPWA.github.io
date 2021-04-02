4e10538e526657d4555a84c4963f5d1911e39177
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2021-04-02 16:40:36 -0400

    Update push_service_sw.js

--- push_service/push_service_sw.js
+++ push_service/push_service_sw.js
@@ -5,7 +5,16 @@ self.addEventListener('activate', (e) => {
          caches.keys().then(function(cacheNames) {
                return Promise.all(
                      cacheNames.map(function(cacheName) {
-                            console.log(cacheName)
+                            console.log(cacheName)                         
+                            caches.open(cacheName).then(function(cache) {
+                               return cache.match('/images/logo-32x32.png').then(function(response) {
+                                 if (response) {
+                                   console.log(' Found response in cache:', response);
+                                 }
+                               })
+                             })
+//                              const r = await caches.match('/images/logo-32x32.png');
+//                              conosle.log(r)
                      })
                  )
          })
