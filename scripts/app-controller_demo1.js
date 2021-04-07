383fb3b3a3ef6cb0719bb65bb0cd0df919cfd740
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2021-04-07 00:12:02 -0400

    Update app-controller_demo1.js

--- scripts/app-controller_demo1.js
+++ scripts/app-controller_demo1.js
@@ -94,7 +94,7 @@ var AppController = function () {
       // Check that service workers are supported
       if ('serviceWorker' in navigator) {
 //         './'+sw_file
-        navigator.serviceWorker.register('https://gauntface.github.io/simple-push-demo/service-worker.js').catch(function (err) {
+        navigator.serviceWorker.register('./'+sw_file).catch(function (err) {
           _this3.showErrorMessage('Unable to Register SW', 'Sorry this demo requires a service worker to work and it ' + 'failed to install - sorry :(');
           console.error(err);
         });
