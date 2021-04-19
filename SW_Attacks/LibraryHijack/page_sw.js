c7135e73c52477461ab8d43645524c2f46157e53
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2021-04-19 15:30:30 -0400

    Create page_sw.js

---
+++ SW_Attacks/LibraryHijack/page_sw.js
@@ -0,0 +1,27 @@
+//A Service Worker to handle Page resource fetch
+
+const cacheName = 'js13kPWA-v1';
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    if (e.request.url.indexOf('snippets.js')<0){
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+    }
+    return response;
+  })());
+});
+
+self.addEventListener('install', event=>{
+    self.skipWaiting();
+})
+
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
