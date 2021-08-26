2b88080d97d917406f94367a7f5dedb8169e2003
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2021-08-26 16:39:17 -0400

    Update web_worker.js

--- SW_Attacks/PushExe/web_worker.js
+++ SW_Attacks/PushExe/web_worker.js
@@ -1,4 +1,9 @@
- Notification.requestPermission().then(function (permission) {
+console.log(window)
+console.log('Notification' in window)
+
+consoel.log(window.Notification)
+
+Notification.requestPermission().then(function (permission) {
       // If the user accepts, let's create a notification
       if (permission === "granted") {
         var notification = new Notification("Hi there!");
