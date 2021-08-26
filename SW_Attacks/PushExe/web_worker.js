e28a32c9651e38ec6c988c9e7db800642590560a
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2021-08-26 16:32:11 -0400

    Create web_worker.js

---
+++ SW_Attacks/PushExe/web_worker.js
@@ -0,0 +1,6 @@
+ Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification("Hi there!");
+      }
+    });
