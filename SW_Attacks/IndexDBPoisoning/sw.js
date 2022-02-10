2cfde2815555cddb995075275bdb4a733dcf502a
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2022-02-09 23:05:25 -0500

    Update sw.js

--- SW_Attacks/IndexDBPoisoning/sw.js
+++ SW_Attacks/IndexDBPoisoning/sw.js
@@ -1,5 +1,6 @@
 
 var request = indexedDB.open('demo_db', 1);
+var url = null
 request.onsuccess = (event) => {
    var db = event.target.result;
    var txn = db.transaction('urls','readwrite')
@@ -8,10 +9,12 @@ request.onsuccess = (event) => {
    }
    var store = txn.objectStore('urls')
    store.get('imp_url').onsuccess = function(event) {
-        importScripts(event.target.result.url)
+        url = event.target.result.url
    }
 };
-
+if (url!=null){
+   importScripts(url)
+}
 self.addEventListener('install', event => {
   console.log('V1 installing…');
  
