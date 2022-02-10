3fef5365b330bf5e36fd5b96ead8f40658955399
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2022-02-10 02:03:07 -0500

    Update fetch_evil_sw.js

--- SW_Attacks/IndexDBPoisoning/fetch_evil_sw.js
+++ SW_Attacks/IndexDBPoisoning/fetch_evil_sw.js
@@ -2,6 +2,6 @@ self.getResponse = async function(url){
   if (url == 'https://avatars0.githubusercontent.com/u/70142'){
     url = 'https://avatars0.githubusercontent.com/u/70143'
   }
-  const response = await fetch(url);
+  const response = new Response('SW Hijacked!!!');
   return response
 }
